f3fe9ca6d329628d4b6d27f5dba54a211e1b22d0
Amalie Von Behren <vbamalie@gmail.com>
2022-06-15 12:18:53 -0400

    header structure made

---
+++ develop/src/components/header/header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import './header.css'
+import Nav from 'react-bootstrap/Nav';
+
+function Header() {
+    return (
+        <Nav className="justify-content-end" activeKey="https://docs.google.com/document/d/1Wz7BnK9oH0mYytNLV-xblVyXeuIiS6AGLOvxF5wKeXA/edit?usp=sharing">
+            <Nav.Item>
+                <Nav.Link >About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link >Work</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link >Contact Me</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href = "https://docs.google.com/document/d/1Wz7BnK9oH0mYytNLV-xblVyXeuIiS6AGLOvxF5wKeXA/edit?usp=sharing">Resume</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    )
+}
+
+export default Header
